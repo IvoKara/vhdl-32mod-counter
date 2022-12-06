fb99ab896f3081c5945adc6b8b8f325fc4b2ed0c
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2022-12-06 16:11:59 +0200

    Modify counting logic

--- Counter_32.vhd
+++ Counter_32.vhd
@@ -23,34 +23,38 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 
-
 entity ccc is
+generic(
+	BITS: positive := 5;
+	POW_OF_2: positive := 3
+);
 port (
-	DIR: in std_logic;
-	CLK_MODE: in std_logic;
-	CLK_MANUAL: in std_logic;
-	CLK_INTERNAL: in std_logic;
-	CLR: in std_logic;
-	Q: inout std_logic_vector (4 downto 0):= "00000"
+	DIR: in std_logic; -- dircation: '1' upwards, '0' downwards
+	CLK_MODE: in std_logic; -- clock mode: '1' automatic, '0' manual
+	CLK_MANUAL: in std_logic; -- clock signal, fired by hand
+	CLK_INTERNAL: in std_logic; -- 100Mhz generator from board
+	CLR: in std_logic; -- async reset signal
+	Q: inout std_logic_vector (BITS-1 downto 0):= (others => '0') -- output
 );
 end ccc;
 
 
 architecture Behavioral of ccc is
 	signal clock: std_logic := '0';
-	signal count: std_logic_vector(4 downto 0) := "00000";
+	signal count: std_logic_vector(BITS-1 downto 0):= (others => '0');
+	signal maxCount: std_logic_vector(BITS-1 downto 0):= (others => '1');
 begin
 
 	process(CLR, CLK_MANUAL, CLK_MODE, CLK_INTERNAL)
-		constant modulus: integer := 100000000; -- because of 100MHz internal clock
-		variable clockCount: natural range 0 to modulus-1 := 0;
+		constant clockModulus: integer := 100000000; -- because of 100MHz internal clock
+		variable clockCount: natural range 0 to clockModulus-1 := 0;
 	begin
 		if (CLR = '1') then 
 			clockCount := 0;
 			clock <= '0';
 		elsif(CLK_MODE = '1') then
 			if(CLK_INTERNAL'event and CLK_INTERNAL = '1') then
-				if clockCount=modulus-1 then
+				if (clockCount = clockModulus - 1) then
 					clockCount := 0;
 					clock <= '1';
 				else 
@@ -66,16 +70,24 @@ begin
 	process (CLR, clock, DIR)
 	begin
 		if (CLR = '1') then
-			count <= (others=>'0');
+			count <= (others => '0');
 		elsif (clock'event and clock = '1') then
 			if (DIR = '1') then
-				count <= count - '1';
+				if (count = "00000") then
+					count <= maxCount;
+				else
+					count <= count - '1';
+				end if;
 			else
-				count <= count + '1';
+				if (count = maxCount) then
+					count <= (others => '0');
+				else
+					count <= count + '1';
+				end if;
 			end if;
 		end if;
 	end process;
 	
-	Q 	<= count;
+	Q <= count;
 	
 end Behavioral;
